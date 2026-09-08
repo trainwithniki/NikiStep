c822f507bf2a6dd570a439074dd1e2fba3e5b2f5
Svetlicha <svetlichaa@gmail.com>
2026-09-08 11:27:13 +0300

    Move profile management into separate admin tab

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='niki-apps-v14';
+const CACHE_NAME='niki-apps-v15';
 const APP_SHELL=[
   './',
   './index.html',
@@ -9,8 +9,8 @@ const APP_SHELL=[
   './modal-scroll-lock.js?v=20260822-1',
   './config.js',
   './supabase-sync.js?v=20260908-2',
-  './admin-history.css?v=20260908-1',
-  './admin-history.js?v=20260908-2',
+  './admin-history.css?v=20260908-2',
+  './admin-history.js?v=20260908-3',
   './admin-pwa-install.js?v=20260823-2',
   './assets/admin-icons/admin-icon-192.png',
   './assets/admin-icons/admin-icon-512.png',
